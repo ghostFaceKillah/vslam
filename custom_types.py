0a09a2748103cdd5cc9b03082ae72d26e0b77599
ghostFaceKillah <garmulewicz@gmail.com>
2022-11-20 14:23:22 -0800

    resolve typing mess

--- custom_types.py
+++ custom_types.py
@@ -6,45 +6,4 @@ from numpy.typing import ArrayLike
 DirPath = str
 FilePath = str
 
-
-Shape = TypeVar("Shape")
-DType = TypeVar("DType")
-
-
-# class _Array(np.ndarray, Generic[Shape, DType]):
-#     """ Generic type used in type annotations. For example,
-#     x: Array['N,K,2', np.int]
-#     is an np ndarray of `np.int`s of shape N, K, 2
-#     """
-#     pass
-#
-
-
-# Array = Union[_Array, np.ndarray]
-
-# class Array(Generic[Shape, DType], Union[_Array[Shape, DType], np.ndarray]):
-#     pass
-
-
-# class Array(ArrayLike, Generic[Shape, DType]):
-#     pass
-
-# class Array(Generic[Shape, DType], ArrayLike):
-#     pass
-
-# class Array(Generic[Shape, DType], np.ndarray):
-#     pass
-
-
-Array = np.ndarray
-
-"""
-
-What do we want ???
-
-- if we have function that wants Array['12', np.int], passing in np.ndarray is OK
-- supports the generic syntax
-
-
-
-"""
+Array: TypeAlias = np.ndarray
