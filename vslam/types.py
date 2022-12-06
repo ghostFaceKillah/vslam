f3fdfbfdec42c052728c7b285a17047bb4de394b
ghostFaceKillah <garmulewicz@gmail.com>
2022-12-05 21:43:42 -0800

    add more details around types

--- vslam/types.py
+++ vslam/types.py
@@ -3,10 +3,10 @@ import numpy as np
 
 from utils.custom_types import Array
 
-
 Vector3d = Array['3', np.float64]
 
 CameraIntrinsicMatrix = Array['3,3', np.float64]
+Cam3dHomogToPxMatrix = Array['3,2', np.float64]
 
 
 @attr.s(auto_attribs=True)
@@ -19,10 +19,18 @@ class CameraIntrinsics:
     def to_matrix(self) -> CameraIntrinsicMatrix:
         return np.array([
             [self.fx, 0, self.cx],
-            [0, self.fy, self.fx],
+            [0, self.fy, self.cy],
             [0, 0, 1]
         ], dtype=np.float64)
 
+    def get_homo_cam_coords_to_px_coords_matrix(self):
+        # CamCoords3dHomog to PxCoords2d matrix
+        return np.array([
+            [0, self.fy, self.cy],
+            [self.fx, 0, self.cx],
+        ], dtype=np.float64)
+
+    # TODO: Probably need a matrix that includes the flip - see coordinate definitions
 
 """
 perception equation in the camera frame
@@ -47,27 +55,31 @@ ZP_uv =  Z *  [v]  = K(R P_w + T) = KTP
 
 
 CameraPoseSE3 = Array['4,4', np.float64]
+TransformSE3 = Array['4,4', np.float64]
 CameraRotationSO3 = Array['3,3', np.float64]
 CameraTranslationVector = Array['3', np.float64]
+Vector3dHomogenous = Array['4', np.float64]   # TODO: nomenclature difference between having 0 on last coordinate or not
 
-WorldCoords3D = Array['N,3', np.float64]      # starts at world origin, assume normalized unit image plane in front
-CamCoords3d = Array['N,3', np.float64]        # x goes right, y down, z out, looking out of cam
+
+WorldCoords3D = Array['N,3', np.float64]      # x away from us, y to right, z up, starts at world origin
+CamCoords3d = Array['N,3', np.float64]        # x goes right, y down, z out, looking out of cam, origin is optical center
 CamCoords3dHomog = Array['N,3', np.float64]   # X/Z Y/Z 1
-ImgCoords3d = Array['N,3', np.float64]        # x goes right, y down, z out of cam; z*u, z*v, z
-ImgCoords2d = Array['N,2', np.float64]        # x goes right, y down, z out of cam; homogenous without z,
+ImgCoords2d = Array['N,2', np.float64]        # x goes right, y down, homogenous without z,
 PxCoords2d = Array['N,2', np.int64]           # int, x goes down, y goes right
 Cv2PxCoords2d = Array['N,2', np.int64]        # int, x goes right (todo left ?), y goes down
 
 """
 WorldCoords3D
-    -[CameraPoseSE3 * _]->
-        CamCoords3d
-            -[/Z] -> 
-                CamCoords3dHomog 
-                    -[undistortion]->
-                            ImgCoords3d
-                                -[drop third dim]->
-                                    ImgCoords2d
-                                        -[-center, *px scale and focal (intrinsics)]
-                                            -> PxCoords2d
+    -[coordinate flip, x = y, y = -z, z = x] -> 
+        CamFlippedWorldCoords3D
+            -[CameraPoseSE3 * point coords]->
+                CamCoords3d
+                    -[/Z] -> 
+                        CamCoords3dHomog 
+                            -[undistortion]->
+                                    ImgCoords3d
+                                        -[drop third dim]->
+                                            ImgCoords2d
+                                                -[-center, *px scale and focal (intrinsics), flip (!)]
+                                                    -> PxCoords2d
 """
