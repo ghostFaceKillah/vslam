e7a86df653527c81f1562b843b89bf9c5f36e7aa
ghostFaceKillah <garmulewicz@gmail.com>
2023-01-26 22:01:47 -0800

    new sparser scene sampling

--- sim/sample_scenes.py
+++ sim/sample_scenes.py
@@ -64,6 +64,18 @@ def get_two_triangle_scene() -> List[RenderTriangle3d]:
     ]
 
 
+def _get_triangle_from_center(
+    rng: onp.random.RandomState,
+    center: Vector3d,
+    radius: float,
+):
+    pre_vertices = rng.normal(size=(3, 3))
+
+    normalized_vertices = pre_vertices / np.linalg.norm(pre_vertices, axis=-1)[..., np.newaxis]
+    vertices = (normalized_vertices * radius) + center
+    return homogenize(vertices)
+
+
 def _get_triangle_vertices(
     rng: onp.random.RandomState,
     radius: float,
@@ -114,3 +126,47 @@ def get_triangles_in_sky_scene(
             )
 
     return triangles
+
+
+def get_triangles_in_sky_scene_2(
+        rng: onp.random.RandomState = onp.random.RandomState(42),
+        min_z: float = 0.5,
+        max_z: float = 10.0,
+        min_x: float = -20.0,
+        max_x: float = 20.0,
+        min_y: float = -20.0,
+        max_y: float = 20.0,
+) -> List[RenderTriangle3d]:
+
+    xs = np.linspace(min_x, max_x, num=10)
+    ys = np.linspace(min_y, max_y, num=10)
+    zs = np.linspace(min_z, max_z, num=5)
+
+    probability_exists = 0.8
+
+    colors = [
+        BGRCuteColors.OFF_WHITE, BGRCuteColors.PURPLE, BGRCuteColors.CRIMSON,
+        BGRCuteColors.SALMON, BGRCuteColors.ORANGE, BGRCuteColors.SUN_YELLOW,
+        BGRCuteColors.GRASS_GREEN, BGRCuteColors.TURQUOISE, BGRCuteColors.VIOLET
+    ]
+
+    sizes = [0.3, 1., 3.]
+
+    triangles = []
+
+    for x, y in itertools.product(xs, ys):
+        if rng.random() < probability_exists:
+            z = rng.choice(zs)
+
+            size = rng.choice(sizes)
+
+            triangle_points = _get_triangle_from_center(rng, center=np.array([x, y, z], dtype=np.float64), radius=size)
+            color = colors[rng.randint(len(colors))]
+
+            triangle = RenderTriangle3d(triangle_points, color, color)
+            triangles.append(triangle)
+
+    return triangles
+
+if __name__ == '__main__':
+    get_triangles_in_sky_scene_2()
