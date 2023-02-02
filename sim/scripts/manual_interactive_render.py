bc94ed5c30fe4075ffed9df18a4e5183fd6f66e5
ghostFaceKillah <garmulewicz@gmail.com>
2023-02-01 21:23:41 -0800

    refactor manual interactor rendere

--- sim/scripts/manual_interactive_render.py
+++ sim/scripts/manual_interactive_render.py
@@ -1,50 +1,122 @@
+from typing import List
+
+import attr
 import cv2
 import numpy as onp
 from jax import numpy as np
 
 from plotting import Col, Padding, Row, TextRenderer
-from sim.birds_eye_view_render import get_view_spcifier_from_scene, render_birdseye_view
+from sim.birds_eye_view_render import get_view_spcifier_from_scene, render_birdseye_view, BirdseyeViewParams
 from sim.clipping import ClippingSurfaces
 from sim.egocentric_render import render_scene_pixelwise_depth
 from sim.sample_scenes import get_triangles_in_sky_scene_2
-from sim.ui import key_to_maybe_transforms
+from sim.sim_types import RenderTriangle3d
+from sim.ui import key_to_maybe_transforms, UiRequestedTransforms
 from utils.colors import BGRCuteColors
-from utils.custom_types import BGRImageArray
+from utils.custom_types import BGRImageArray, BGRColor
 from utils.image import magnify
 from utils.profiling import just_time
 from vslam.math import normalize_vector
 from vslam.poses import get_SE3_pose
-from vslam.types import CameraIntrinsics, CameraPoseSE3
+from vslam.types import CameraIntrinsics, CameraPoseSE3, Vector3d
 
-if __name__ == '__main__':
-    screen_h = 480
-    screen_w = 640
 
-    sky_color = BGRCuteColors.SKY_BLUE
-    ground_color = tuple(x - 20 for x in BGRCuteColors.CYAN)
+@attr.define
+class CameraSpecs:
+    screen_h: int
+    screen_w: int
+    cam_intrinsics: CameraIntrinsics
+    clipping_surfaces: ClippingSurfaces
 
-    # higher f_mod -> less distortion, less field of view
-    f_mod = 2.0
-    distance_between_eyes = 2.0
+    @classmethod
+    def from_default(
+        cls,
+        screen_h: int = 480,
+        screen_w: int = 640,
+        f_mod: float = 2.0,   # higher f_mod -> less distortion, less field of view
+    ):
+        cam_intrinsics = CameraIntrinsics(
+            fx=screen_w / 4 * f_mod,
+            fy=screen_h / 3 * f_mod,
+            cx=screen_w / 2,
+            cy=screen_h / 2,
+        )
+        return cls(
+            screen_h=screen_h,
+            screen_w=screen_w,
+            cam_intrinsics=cam_intrinsics,
+            clipping_surfaces=ClippingSurfaces.from_screen_dimensions_and_cam_intrinsics(screen_h, screen_w, cam_intrinsics),
+        )
+
+
+@attr.define
+class TriangleSceneRenderer:
+    scene_triangles: List[RenderTriangle3d]
+    birdseye_view_specifier: BirdseyeViewParams
+
+    camera: CameraSpecs = attr.field(factory=CameraSpecs.from_default)
+    distance_between_eyes: float = 2.0
+
+    light_direction_in_world: Vector3d = normalize_vector(np.array([1.0, -1.0, -8.0]))
 
     shade_color = BGRCuteColors.DARK_GRAY
-    cam_intrinsics = CameraIntrinsics(
-        fx=screen_w / 4 * f_mod,
-        fy=screen_h / 3 * f_mod,
-        cx=screen_w / 2,
-        cy=screen_h / 2,
-    )
-    light_direction = normalize_vector(np.array([1.0, -1.0, -8.0]))
-    clipping_surfaces = ClippingSurfaces.from_screen_dimensions_and_cam_intrinsics(screen_h, screen_w, cam_intrinsics)
+    sky_color: BGRColor = BGRCuteColors.SKY_BLUE
+    ground_color: BGRColor = tuple(x - 20 for x in BGRCuteColors.CYAN)
+
+    @classmethod
+    def from_default(cls):
+
+        # triangles = get_two_triangle_scene()
+        # triangles = get_cube_scene()
+        # triangles = get_triangles_in_sky_scene()
+        triangles = get_triangles_in_sky_scene_2()
+
+        return cls(
+            scene_triangles=triangles,
+            birdseye_view_specifier=get_view_spcifier_from_scene(triangles),
+        )
+
+    def render_first_person_view(self, camera_pose: CameraPoseSE3) -> BGRImageArray:
+        """ Renders the scene from the perspective of the camera """
+        return render_scene_pixelwise_depth(
+            self.camera.screen_h,
+            self.camera.screen_w,
+            camera_pose,
+            self.scene_triangles,
+            self.camera.cam_intrinsics,
+            self.light_direction_in_world,
+            self.sky_color,
+            self.ground_color,
+            self.shade_color,
+            self.camera.clipping_surfaces
+        )
+
+    def render_birdseye_view(self, camera_pose: CameraPoseSE3) -> BGRImageArray:
+        """ Renders the scene from birdseye view """
+        return render_birdseye_view(
+            self.camera.screen_h,
+            self.camera.screen_w,
+            self.birdseye_view_specifier,
+            camera_pose,
+            self.camera.cam_intrinsics,
+            self.scene_triangles,
+            self.ground_color
+        )
+
+    def left_eye_offset(self):
+        return get_SE3_pose(y=-self.distance_between_eyes / 2)
+
+    def right_eye_offset(self):
+        return get_SE3_pose(y=self.distance_between_eyes / 2)
+
+
+if __name__ == '__main__':
+    scene_renderer = TriangleSceneRenderer.from_default()
 
     # looking toward +x direction in world frame, +z in camera
     camera_pose: CameraPoseSE3 = get_SE3_pose(x=-2.5)
 
-    # triangles = get_two_triangle_scene()
-    # triangles = get_cube_scene()
-    # triangles = get_triangles_in_sky_scene()
-    triangles = get_triangles_in_sky_scene_2()
-    view_specifier = get_view_spcifier_from_scene(triangles)
+
     text_renderer = TextRenderer()
 
     layout = Col(
@@ -54,31 +126,22 @@ if __name__ == '__main__':
     )
 
     i = 0
-
-    # just to make rendering loop prettier
-    def _packed_render_function(camera_pose_: CameraPoseSE3) -> BGRImageArray:
-        return render_scene_pixelwise_depth(
-            screen_h, screen_w, camera_pose_, triangles,
-            cam_intrinsics, light_direction, sky_color,
-            ground_color, shade_color, clipping_surfaces
-        )
-
+    transforms = UiRequestedTransforms.empty()
 
     while True:
-        with just_time('right render'):
-            right_eye_screen = _packed_render_function(camera_pose @ get_SE3_pose(y=distance_between_eyes / 2))
-            left_eye_screen = _packed_render_function(camera_pose @ get_SE3_pose(y=-distance_between_eyes / 2))
+        if transforms.camera is not None:
+            camera_pose = camera_pose @ transforms.camera
 
-        bev_img = render_birdseye_view(
-            screen_h=screen_h,
-            screen_w=screen_w,
-            view_specifier=view_specifier,
-            camera_pose=camera_pose,
-            camera_intrinsics=cam_intrinsics,
-            triangles=triangles,
-            bg_color=ground_color
-        )
+        with just_time('eyes render'):
+            with just_time('right eye render'):
+                right_eye_screen = scene_renderer.render_first_person_view(camera_pose @ scene_renderer.right_eye_offset())
+            with just_time('left eye render'):
+                left_eye_screen = scene_renderer.render_first_person_view(camera_pose @ scene_renderer.left_eye_offset())
 
+        with just_time('birdseye render'):
+            bev_img = scene_renderer.render_birdseye_view(camera_pose)
+
+        # react
         img = layout.render({
             'desc': text_renderer.render(f'frame {i} pose {camera_pose[:3, 3]}'),
             'left_img': left_eye_screen,
@@ -88,18 +151,10 @@ if __name__ == '__main__':
 
         cv2.imshow('scene', onp.array(img))
         key = cv2.waitKey(-1)
+        transforms = key_to_maybe_transforms(key)
 
         if key == 27:
             print("caught escape key, exiting")
             break
-
-        # mutate state based on keys
-        transforms = key_to_maybe_transforms(key)
-
-        if transforms.scene is not None:
-            triangles = [triangle.mutate(transforms.scene) for triangle in triangles]
-
-        if transforms.camera is not None:
-            camera_pose = camera_pose @ transforms.camera
 
         i += 1
