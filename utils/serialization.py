aac6b188ed6d3ca01921fef8509c0fe191863cdd
ghostFaceKillah <garmulewicz@gmail.com>
2023-02-02 21:01:39 -0800

    serializatiorn pushed a bit

---
+++ utils/serialization.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+import msgpack
+import msgpack_numpy as m
+import numpy as onp
+
+
+def msgpack_dumps(obj: Any) -> bytes:
+    return msgpack.packb(obj, use_bin_type=True, default=m.encode)
+
+
+def msgpack_loads(data: bytes):
+    return msgpack.unpackb(data, raw=False, object_hook=m.decode)
+
+
+def experiment(obj):
+
+    the_in = msgpack.packb(obj, use_bin_type=True, default=m.encode)
+    the_out = msgpack.unpackb(the_in, raw=False, object_hook=m.decode)
+
+    print(f"{the_in=}")
+    print(f"{the_out=}")
+
+
+if __name__ == '__main__':
+    experiment([1, 2, 3])
+
+    an_array_ok = onp.array([1, 2, 3, 4], onp.float32)
+    experiment(an_array_ok)
+
