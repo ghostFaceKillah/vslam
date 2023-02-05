ecd9656d1bf54cf5b1c871f0365e090d5615e138
ghostFaceKillah <garmulewicz@gmail.com>
2023-02-04 16:01:03 -0800

    Add file utils

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -22,16 +22,53 @@ def mkdir_p(path: str):
             raise
 
 
+
+
+def ensure_parent_dir_exists(file_path: str) -> None:
+    """ Ensure that the parent directory for the file exists """
+    parent_dir, _ = os.path.split(file_path)
+    mkdir_p(parent_dir)
+
+
+def expand_path(*file_path_parts) -> str:
+    """ Expand the user directory and join the parts """
+    file_path = os.path.join(*file_path_parts)
+    file_path = os.path.abspath(os.path.expanduser(file_path))
+    return file_path
+
+
+def ensure_path(*file_path_parts) -> str:
+    """ Ensure that the path results in a writable file.  This includes:
+    - Expanding ~ into userdir
+    - Turning relative paths into absolute paths
+    - Ensuring parent dir exists
+    """
+    file_path = expand_path(*file_path_parts)
+    ensure_parent_dir_exists(file_path)
+    return file_path
+
+
 def easy_filename(
         slug: str,
         where: str = _DEFAULT_DATA_DIR,
-        ext: str = "dat",
+        ext: str = "",
         randomize: bool = False,
 ) -> str:
     """ Creates a (unique if needed) filename """
+    if '.' in slug and len(ext) > 0:
+        raise ValueError("Slug should not contain an extension if you are specifying one")
+
+    if '.' in slug:
+        slug, ext = slug.split('.')
+
+    extension_or_none = f".{ext}" if len(ext) > 0 else ""
+
     if randomize:
         random_str = uuid.uuid4().hex
-        return os.path.join(where, f"{slug}_{get_now_datetime_as_string()}_{random_str}.{ext}")
+        cand = f"{slug}_{get_now_datetime_as_string()}_{random_str}" + extension_or_none
     else:
-        return os.path.join(where, f"{slug}_{get_now_datetime_as_string()}.{ext}")
+        cand = f"{slug}_{get_now_datetime_as_string()}" + extension_or_none
+
+    return ensure_path(where, cand)
+
 
