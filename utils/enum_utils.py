73d988288600e6f1c34f124da1c0647d7f53aaa2
ghostFaceKillah <garmulewicz@gmail.com>
2023-04-18 17:26:13 -0700

    Better

--- utils/enum_utils.py
+++ utils/enum_utils.py
@@ -3,22 +3,3 @@ from enum import Enum
 
 class StrEnum(str, Enum):
     ...
-
-
-
-class Example(StrEnum):
-    OKE = 'oke'
-    WER = 'wer'
-
-
-
-class A:
-    def __init__(self, *args):
-        x = 1
-
-if __name__ == '__main__':
-    # print(Example.WER)
-    # print(f"'wer' == Example.WER => {Example.WER == 'wer'}")
-    # print(f"Example.WER == Example.WER => {Example.WER == Example.WER}")
-    # print(f"Example.WER == Example.OKE => {Example.WER == Example.OKE}")
-    a = A(1, 2,3, 4)
