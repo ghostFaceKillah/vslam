6e57dda11dd1d1e27547bb8c4f69eaac3fbbbe50
ghostFaceKillah <garmulewicz@gmail.com>
2023-04-18 17:23:55 -0700

    autoformat

--- lessons/ex_03_estimate_current_pose.py
+++ lessons/ex_03_estimate_current_pose.py
@@ -25,7 +25,12 @@ import numpy as np
 from liegroups.numpy.se3 import SE3Matrix
 from sim.sample_scenes import get_two_triangle_scene
 from utils.profiling import just_time
-from vslam.pnp import estimate_J_numerically, estimate_J_analytically, gauss_netwon_pnp, _compute_reprojection_error
+from vslam.pnp import (
+    estimate_J_numerically,
+    estimate_J_analytically,
+    gauss_netwon_pnp,
+    _compute_reprojection_error,
+)
 from vslam.poses import get_SE3_pose
 from vslam.transforms import SE3_inverse, get_world_to_cam_coord_flip_matrix
 from vslam.types import CamFlippedWorldCoords3D, CameraPoseSE3, ImgCoords2d
@@ -40,7 +45,7 @@ class _PoseEstimationData:
     points_2d_in_img: ImgCoords2d
 
     @classmethod
-    def example(cls) -> '_PoseEstimationData':
+    def example(cls) -> "_PoseEstimationData":
         triangles = get_two_triangle_scene()
 
         # our initial, incorrect guess
@@ -56,7 +61,9 @@ class _PoseEstimationData:
         points_3d_in_world = np.concatenate([tri.points for tri in triangles])
         points_3d_in_keyframe = (SE3_inverse(keyframe_pose) @ points_3d_in_world.T).T
 
-        points_in_cam_homogenous = (world_to_cam_flip @ SE3_inverse(true_camera_pose) @ points_3d_in_world.T).T
+        points_in_cam_homogenous = (
+            world_to_cam_flip @ SE3_inverse(true_camera_pose) @ points_3d_in_world.T
+        ).T
         points_in_cam = points_in_cam_homogenous[..., :-1]
         point_depths = points_in_cam[..., -1]
         points_2d_in_img = (points_in_cam / point_depths[..., np.newaxis])[..., :-1]
@@ -66,12 +73,12 @@ class _PoseEstimationData:
             camera_pose_initial_guess=cam_pose_initial_guess,
             keyframe_pose=keyframe_pose,
             points_3d_in_keyframe=points_3d_in_keyframe,
-            points_2d_in_img=points_2d_in_img
+            points_2d_in_img=points_2d_in_img,
         )
 
 
 def _experiment_what_is_the_meaning_of_the_axes():
-    """ Check if `liegroups` has the same conventions around coordinates as we do. """
+    """Check if `liegroups` has the same conventions around coordinates as we do."""
 
     data = _PoseEstimationData.example()
 
@@ -95,9 +102,10 @@ def _experiment_what_is_the_meaning_of_the_axes():
 
 
 def _overfit_one_point():
-    """ Make sure whether Jacobian is useful at all (doesn't contain obvious bugs).
+    """Make sure whether Jacobian is useful at all (doesn't contain obvious bugs).
     We consider a drastically simplified setting of minimizing reprojection error for one point only.
-    Backpropagating through negated Jacobian should result in gradual lowering of error and convergence. """
+    Backpropagating through negated Jacobian should result in gradual lowering of error and convergence.
+    """
     data = _PoseEstimationData.example()
 
     point_3d = data.points_3d_in_keyframe[0]
@@ -122,8 +130,9 @@ def _overfit_one_point():
 
 
 def _solve_many_points_first_order_descent(verbose: bool = True):
-    """ Naive method: numerical derivative, direct gradient.
-    On the way there, make sure that analytic and numeric jacobians are close to each other. """
+    """Naive method: numerical derivative, direct gradient.
+    On the way there, make sure that analytic and numeric jacobians are close to each other.
+    """
     data = _PoseEstimationData.example()
 
     camera_pose = data.camera_pose_initial_guess
@@ -147,12 +156,14 @@ def _solve_many_points_first_order_descent(verbose: bool = True):
         dx_est = np.array(dxs).mean(axis=0)
         mean_abs_jac_err = np.array(jac_err).mean()
         loss = np.linalg.norm(np.array(errs), axis=1).mean()
-        camera_pose = camera_pose @ SE3Matrix.exp(- dx_est).as_matrix()
+        camera_pose = camera_pose @ SE3Matrix.exp(-dx_est).as_matrix()
 
         assert mean_abs_jac_err < 1e-4
 
         if verbose and i % 10 == 0:
-            print(f"i = {i} mse = {loss:.2f} dx = {dx_est.round(2)} jac_err = {mean_abs_jac_err:.5f}")
+            print(
+                f"i = {i} mse = {loss:.2f} dx = {dx_est.round(2)} jac_err = {mean_abs_jac_err:.5f}"
+            )
 
     print("Final pose estimate is")
     print(camera_pose.round(2))
@@ -164,7 +175,7 @@ def _solve_many_points_first_order_descent(verbose: bool = True):
 
 
 def _solve_many_points_gauss_newton(verbose: bool = False):
-    """ Pose estimation that is somewhat close to production:
+    """Pose estimation that is somewhat close to production:
     1) Gauss newton second order method
     2) analytical gradient
     not vectrorized though!
@@ -176,7 +187,7 @@ def _solve_many_points_gauss_newton(verbose: bool = False):
         data.camera_pose_initial_guess,
         data.points_3d_in_keyframe,
         data.points_2d_in_img,
-        verbose=True
+        verbose=True,
     )
 
     print("Final pose estimate is")
@@ -188,7 +199,7 @@ def _solve_many_points_gauss_newton(verbose: bool = False):
     print((camera_pose - data.camera_pose).round(2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # _experiment_what_is_the_meaning_of_the_axes()
     # _overfit_one_point()
 
@@ -200,5 +211,3 @@ if __name__ == '__main__':
 
     # I can do it numerically for fun
     # obvious bug - reprojecting the native camera results in error, wtf
-
-
--- lessons/ex_04_manual_pnp_on_our_data.py
+++ lessons/ex_04_manual_pnp_on_our_data.py
@@ -20,13 +20,15 @@ def _process_debug_info(
 ) -> BGRImageArray:
     # TODO: move all of this inside Localization debugger ?
     print(iteration_number)
-    print(f"est pose = {SE3_pose_to_xytheta(frontend_resu.baselink_pose_estimate).round(2)}")
+    print(
+        f"est pose = {SE3_pose_to_xytheta(frontend_resu.baselink_pose_estimate).round(2)}"
+    )
     print(f"gt  pose = {SE3_pose_to_xytheta(obs.baselink_pose).round(2)}")
 
     if frontend_resu.debug_data.frames_since_keyframe == 0:
         localization_debugger.add_keyframe(
             keyframe_baselink_pose=frontend_resu.baselink_pose_estimate,
-            keyframe_img=obs.left_eye_img
+            keyframe_img=obs.left_eye_img,
         )
 
     localization_debugger.add_pose_estimate(
@@ -35,27 +37,28 @@ def _process_debug_info(
         current_left_eye_image=obs.left_eye_img,
         current_right_eye_image=obs.right_eye_img,
         feature_matches_or_none=frontend_resu.debug_data.feature_matches,
-        frames_since_keyframe=frontend_resu.debug_data.frames_since_keyframe
+        frames_since_keyframe=frontend_resu.debug_data.frames_since_keyframe,
     )
     return localization_debugger.render()
 
 
 def run_couple_first_frames():
     # dataset_path = os.path.join(ROOT_DIR, 'data/short_recording_2023-04-01--22-41-24.msgpack')   # short
-    dataset_path = os.path.join(ROOT_DIR, 'data/short_recording_2023-04-11--19-18-13.msgpack')   # long
+    dataset_path = os.path.join(
+        ROOT_DIR, "data/short_recording_2023-04-11--19-18-13.msgpack"
+    )  # long
     data_streamer = SimDataStreamer.from_dataset_path(dataset_path=dataset_path)
 
     np.set_printoptions(suppress=True)  # TODO: remove
 
     localization_debugger = LocalizationDebugger.from_scene(
-        scene=data_streamer.recorded_data.scene,
-        cam_specs=data_streamer.get_cam_specs()
+        scene=data_streamer.recorded_data.scene, cam_specs=data_streamer.get_cam_specs()
     )
 
     frontend = Frontend.from_defaults(
         cam_specs=data_streamer.get_cam_specs(),
         start_pose=get_SE3_pose(x=-2.5),
-        debug_data=FrontendStaticDebugData(scene=data_streamer.recorded_data.scene)
+        debug_data=FrontendStaticDebugData(scene=data_streamer.recorded_data.scene),
     )
 
     for i, obs in enumerate(data_streamer.stream()):
@@ -64,10 +67,9 @@ def run_couple_first_frames():
         debug_img = _process_debug_info(i, frontend_resu, obs, localization_debugger)
 
         # TODO: just_show ?
-        cv2.imshow('localization_debugger', debug_img)
+        cv2.imshow("localization_debugger", debug_img)
         cv2.waitKey(-1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_couple_first_frames()
-
